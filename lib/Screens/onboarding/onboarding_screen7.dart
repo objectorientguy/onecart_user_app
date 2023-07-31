e9d47fa47f7c6966317544461bf53c1e3956062d
vaishnavichafle <vaishnavic16@gmail.com>
2023-07-31 17:40:18 +0530

    Implemented onboarding flow UI.

--- lib/Screens/onboarding/onboarding_screen7.dart
+++ lib/Screens/onboarding/onboarding_screen7.dart
@@ -1,15 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:onecart_user_app/Screens/onboarding/onboarding_screen5.dart';
+import 'package:onecart_user_app/configs/app_color.dart';
 class OnboardingScreen7 extends StatelessWidget {
   const OnboardingScreen7({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      backgroundColor: AppColor.white,
+      appBar: AppBar(
+        leading: IconButton(onPressed: (){
+          Navigator.of(context).pushReplacement(MaterialPageRoute(
+              builder: (context) => const OnboardingScreen5()));
+        },
+            icon: Icon(Icons.arrow_back_rounded,color: AppColor.black,)),
+      ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Image.asset('assets/welcome.png')
+        children:[
+          Image.asset('assets/welcome.png'),
         ],
       ),
     );
