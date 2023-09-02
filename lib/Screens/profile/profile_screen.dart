831e7f6924fa97defba4e1eaae1b67f2303e93f9
Sameer2024 <135789432+Sameer2024@users.noreply.github.com>
2023-09-02 16:32:44 +0530

    OC:121-Fix Account Screen UI
    Co-authored-by: Sameer <gptsameer7@gmail.com>

--- lib/Screens/profile/profile_screen.dart
+++ lib/Screens/profile/profile_screen.dart
@@ -28,19 +28,94 @@ class ProfileScreen extends StatelessWidget {
                       spreadRadius: kZero,
                       blurRadius: kShadowBlurRadius),
                 ]),
+                child: const Padding(
+                  padding: EdgeInsets.symmetric(
+                      horizontal: leftRightMargin, vertical: topBottomPadding),
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        ProfileHeader(),
+                        SizedBox(height: mediumSpacing),
+                        ProfileOptionsList(),
+                        SizedBox(height: tinierSpacing),
+                      ]),
+                ),
+              ),
+              const SizedBox(
+                height: xxSmallerSpacing,
+              ),
+              Container(
+                decoration:
+                    const BoxDecoration(color: AppColor.white, boxShadow: [
+                  BoxShadow(
+                      blurStyle: BlurStyle.solid,
+                      color: AppColor.lightGrey,
+                      spreadRadius: kZero,
+                      blurRadius: kShadowBlurRadius),
+                ]),
                 child: Padding(
                   padding: const EdgeInsets.symmetric(
                       horizontal: leftRightMargin, vertical: topBottomPadding),
                   child: Column(
                       crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
-                        const ProfileHeader(),
-                        const SizedBox(height: mediumSpacing),
-                        const ProfileOptionsList(),
-                        const SizedBox(height: xxxLargerSpacing),
-                        Text('App Version 1.8.0',
-                            style:
-                                Theme.of(context).textTheme.subHeadingMedium),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const Icon(
+                                  Icons.info,
+                                  color: AppColor.primary,
+                                  size: kLocationIconSize,
+                                ),
+                                const SizedBox(
+                                  width: xxTinierSpacing,
+                                ),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text('About Us',
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .textLarge
+                                            .copyWith(
+                                                fontWeight: FontWeight.w700)),
+                                    const SizedBox(
+                                      height: xxTiniestSpacing,
+                                    ),
+                                    SizedBox(
+                                      child: Text(
+                                        'Legal Information',
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .subHeadingMedium,
+                                        overflow: TextOverflow.ellipsis,
+                                      ),
+                                    )
+                                  ],
+                                )
+                              ],
+                            ),
+                            const Row(
+                              children: [
+                                Icon(
+                                  Icons.arrow_forward_ios,
+                                  size: 20,
+                                ),
+                                SizedBox(
+                                  width: xxxTinierSpacing,
+                                )
+                              ],
+                            )
+                          ],
+                        ),
+                        const SizedBox(height: xxSmallerSpacing),
+                        Text(
+                          'App Version 1.8.0',
+                          style: Theme.of(context).textTheme.subHeadingMedium,
+                        ),
                       ]),
                 ),
               ),
