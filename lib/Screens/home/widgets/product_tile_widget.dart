f1f92e4fe0fa505044124803457ddc991a438f43
Sameer <gptsameer7@gmail.com>
2023-09-05 14:30:36 +0530

    OC:126-Implement custom tile for home screen

--- lib/Screens/home/widgets/product_tile_widget.dart
+++ lib/Screens/home/widgets/product_tile_widget.dart
@@ -21,77 +21,80 @@ class ProductTileWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: [
-        Center(
-          child: Container(
-            height: MediaQuery.of(context).size.width * 0.23,
-            width: MediaQuery.of(context).size.width * 0.23,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(kBorderRadiusSmall),
-            ),
-            child: Image.network(
-              data[index].image![0].toString(),
-              fit: BoxFit.fill,
+    return SizedBox(
+      width: MediaQuery.of(context).size.width * 0.23,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Center(
+            child: Container(
+              height: MediaQuery.of(context).size.width * 0.23,
+              width: MediaQuery.of(context).size.width * 0.23,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(kBorderRadiusSmall),
+              ),
+              child: Image.network(
+                data[index].image![0].toString(),
+                fit: BoxFit.fill,
+              ),
             ),
           ),
-        ),
-        const SizedBox(
-          width: xxxTinierSpacing,
-        ),
-        Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  SizedBox(
-                    width: screenwidth * 0.40,
-                    child: Text(
-                      data[index].productName!,
-                      style: Theme.of(context)
-                          .textTheme
-                          .subHeadingMedium
-                          .copyWith(
-                              fontWeight: FontWeight.w500,
-                              color: AppColor.black),
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
-                      softWrap: false,
-                    ),
-                  ),
-                  const Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('4.2'),
-                      SizedBox(
-                        width: xxxTiniestSpacing,
+          const SizedBox(
+            width: xxxTinierSpacing,
+          ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    SizedBox(
+                      width: screenwidth * 0.40,
+                      child: Text(
+                        data[index].productName!,
+                        style: Theme.of(context)
+                            .textTheme
+                            .subHeadingMedium
+                            .copyWith(
+                                fontWeight: FontWeight.w500,
+                                color: AppColor.black),
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                        softWrap: false,
                       ),
-                      Icon(
-                        Icons.star,
-                        size: kStarIcon,
-                        color: Colors.amber,
-                      )
-                    ],
-                  )
-                ],
-              ),
-              const SizedBox(
-                height: tiniestSpacing,
-              ),
-              ProductTileWidgetBody(
-                screenwidth: screenwidth,
-                data: data,
-                index: index,
-              )
-            ],
-          ),
-        )
-      ],
+                    ),
+                    const Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('4.2'),
+                        SizedBox(
+                          width: xxxTiniestSpacing,
+                        ),
+                        Icon(
+                          Icons.star,
+                          size: kStarIcon,
+                          color: Colors.amber,
+                        )
+                      ],
+                    )
+                  ],
+                ),
+                const SizedBox(
+                  height: tiniestSpacing,
+                ),
+                ProductTileWidgetBody(
+                  screenwidth: screenwidth,
+                  data: data,
+                  index: index,
+                )
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
