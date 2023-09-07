d343876219b0201b5a3fb6fbea2fa19f5dd2ad4f
ketan-aplha <78896225+ketan-aplha@users.noreply.github.com>
2023-09-07 17:40:01 +0530

    OC-129: Implemented sort by price filter.
    * OC-129: Implemented filter chips on category screen.
    
    * OC-129: Implemented sort by price filter.
    
    * OC-129: Implemented sort by price filter.

--- lib/Screens/categories/category_item_screen.dart
+++ lib/Screens/categories/category_item_screen.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:onecart_user_app/Screens/categories/widgets/category_screen_header.dart';
 import 'package:onecart_user_app/Screens/categories/widgets/stores_item_list.dart';
 import '../../blocs/get_product_bloc/get_product_bloc.dart';
+import '../../blocs/get_product_bloc/get_product_events.dart';
 import '../../blocs/get_product_bloc/get_product_states.dart';
-import '../../common_widgets/generic_app_bar.dart';
+import '../../configs/app_dimensions.dart';
+import '../../configs/app_spacing.dart';
 
-class CategoryItemScreen extends StatelessWidget {
+class CategoryItemScreen extends StatefulWidget {
   static const routeName = 'CategoryItemScreen';
   final dynamic categoryDetails;
 
@@ -14,31 +15,80 @@ class CategoryItemScreen extends StatelessWidget {
       : super(key: key);
 
   @override
+  State<CategoryItemScreen> createState() => _CategoryItemScreenState();
+}
+
+class _CategoryItemScreenState extends State<CategoryItemScreen> {
+  @override
   Widget build(BuildContext context) {
+    bool price = false;
     return Scaffold(
-      appBar: const GenericAppBar(),
-      body: BlocBuilder<GetProductBloc, GetProductStates>(
-        builder: (context, state) {
+        appBar: AppBar(
+          elevation: kCategoryAppBarElevation,
+          leading: InkWell(
+              child: const Icon(
+                Icons.arrow_back,
+                color: Colors.black,
+              ),
+              onTap: () {
+                Navigator.pop(context);
+              }),
+          title: BlocBuilder<GetProductBloc, GetProductStates>(
+              builder: (context, state) {
+            if (state is FilterPriceLoaded) {
+              return Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(state.getProductByIdModel.category!.categoryName!,
+                        style: const TextStyle(color: Colors.black)),
+                    Row(children: [
+                      SizedBox(
+                        height: kIconSizeSmall,
+                        width: kIconSizeSmall,
+                        child:
+                            Image.asset('assets/filter-filled-tool-symbol.png'),
+                      ),
+                      const SizedBox(width: smallestSpacing),
+                      InkWell(
+                          onTap: () {
+                            context.read<GetProductBloc>().add(SortByPrice(
+                                sortPrice: !price,
+                                categoryModel: state.getProductByIdModel,
+                                productsList: state.productsList));
+                          },
+                          child: SizedBox(
+                              height: kIconSizeSmall,
+                              width: kIconSizeSmall,
+                              child:
+                                  Image.asset('assets/up-and-down-arrow.png')))
+                    ])
+                  ]);
+            }
+            if (state is FetchProductError) {
+              return Container();
+            } else {
+              return const SizedBox();
+            }
+          }),
+        ),
+        body: BlocBuilder<GetProductBloc, GetProductStates>(
+            builder: (context, state) {
           if (state is FetchProductLoading) {
             return const Center(child: CircularProgressIndicator());
-          } else if (state is FetchProductLoaded) {
+          } else if (state is FilterPriceLoaded) {
+            price = state.sortedValue ?? false;
             return Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                CategoryItemScreenHeader(
-                    categorydata: state.getProductByIdModel.category!),
-                StoreItemList(storedata: state.getProductByIdModel.data!),
-              ],
-            );
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  StoreItemList(storedata: state.productsList, toShow: price)
+                ]);
           }
           if (state is FetchProductError) {
             return Container();
           } else {
             return const SizedBox();
           }
-        },
-      ),
-    );
+        }));
   }
 }
--- lib/Screens/categories/widgets/stores_item_list.dart
+++ lib/Screens/categories/widgets/stores_item_list.dart
@@ -6,47 +6,43 @@ import '../../item_details/item_details_screen.dart';
 
 class StoreItemList extends StatelessWidget {
   final List<Deal> storedata;
+  final bool toShow;
 
   const StoreItemList({
     super.key,
     required this.storedata,
+    required this.toShow,
   });
 
   @override
   Widget build(BuildContext context) {
-    double screenwidth = MediaQuery.of(context).size.width;
+    double screenWidth = MediaQuery.of(context).size.width;
     return SingleChildScrollView(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: leftRightMargin),
-        child: Column(
-          children: [
-            const SizedBox(height: 40),
-            ListView.separated(
-              physics: const BouncingScrollPhysics(),
-              shrinkWrap: true,
-              itemCount: storedata.length,
-              itemBuilder: (context, index) {
-                return InkWell(
-                  onTap: () {
-                    Navigator.pushNamed(context, ItemDetailsScreen.routeName,
-                        arguments: storedata[index]);
+        child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: leftRightMargin),
+            child: Column(children: [
+              const SizedBox(height: xMediumSpacing),
+              ListView.separated(
+                  physics: const BouncingScrollPhysics(),
+                  shrinkWrap: true,
+                  itemCount: storedata.length,
+                  itemBuilder: (context, index) {
+                    return InkWell(
+                        onTap: () {
+                          Navigator.pushNamed(
+                              context, ItemDetailsScreen.routeName,
+                              arguments: storedata[index]);
+                        },
+                        child: ProductTileWidget(
+                            screenwidth: screenWidth,
+                            data: storedata,
+                            index: index));
                   },
-                  child: ProductTileWidget(
-                    screenwidth: screenwidth,
-                    data: storedata,
-                    index: index,
-                  ),
-                );
-              },
-              separatorBuilder: (context, index) {
-                return const Divider(
-                  height: xxxSmallSpacing,
-                );
-              },
-            ),
-          ],
-        ),
-      ),
-    );
+                  separatorBuilder: (context, index) {
+                    return const Divider(
+                      height: xxxSmallSpacing,
+                    );
+                  })
+            ])));
   }
 }
