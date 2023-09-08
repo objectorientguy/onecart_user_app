3cb903df02ed52fc779f5a615eb858e3a5767df5
Sameer <gptsameer7@gmail.com>
2023-09-08 18:03:19 +0530

    OC:128-Implement stepper widget in order details screen

--- lib/Screens/orders/order_details_screen.dart
+++ lib/Screens/orders/order_details_screen.dart
@@ -1,10 +1,12 @@
+import 'package:another_stepper/dto/stepper_data.dart';
+import 'package:another_stepper/widgets/another_stepper.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
+
 import 'package:onecart_user_app/common_widgets/generic_app_bar.dart';
 import 'package:onecart_user_app/configs/app_spacing.dart';
-import 'package:onecart_user_app/configs/app_theme.dart';
 
 import '../../configs/app_color.dart';
+
 import '../../configs/app_dimensions.dart';
 import '../../data/models/orders/get_all_orders_model.dart';
 
@@ -16,35 +18,47 @@ class OrdersDetailsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    List orderData = [
-      {
-        'title': 'Order Placed',
-        'status': 'done',
-        'image': 'assets/order_placed.png'
-      },
-      {
-        'title': 'Confirmation of Order',
-        'status': 'done',
-        'image': 'assets/confirmation.png'
-      },
-      {
-        'title': 'Order Shipped',
-        'status': 'current',
-        'image': 'assets/order_shipped.png'
-      },
-      {
-        'title': 'Out for Delivery',
-        'date': 'Pending',
-        'status': 'pending',
-        'image': 'assets/pending.png'
-      },
-      {
-        'title': 'To be Delivered',
-        'date': 'Pending',
-        'status': 'pending',
-        'image': 'assets/pending.png'
-      }
+    List<StepperData> stepperData = [
+      StepperData(
+        title: StepperText(
+          "Ordered",
+        ),
+        subtitle: StepperText("Mon,29 Aug 11:59 AM"),
+        iconWidget: Container(
+            height: kContainer,
+            width: kContainer,
+            decoration: const BoxDecoration(
+                color: AppColor.primaryLighter, shape: BoxShape.circle),
+            child: const Icon(
+              Icons.shopping_bag_outlined,
+              color: AppColor.primary,
+            )),
+      ),
+      StepperData(
+          title: StepperText(
+            "Under Process",
+            textStyle: const TextStyle(
+              color: Colors.grey,
+            ),
+          ),
+          subtitle: StepperText("Mon,29 Aug 11:59 AM"),
+          iconWidget: Image.asset('assets/order_placed.png')),
+      StepperData(
+          title: StepperText(
+            "Shipped",
+            textStyle: const TextStyle(
+              color: Colors.grey,
+            ),
+          ),
+          subtitle: StepperText("Mon,29 Aug 11:59 AM"),
+          iconWidget: Image.asset('assets/order_shipped.png')),
+      StepperData(
+          title: StepperText("Delivered",
+              textStyle: const TextStyle(color: Colors.grey)),
+          subtitle: StepperText("Mon,29 Aug 11:59 AM"),
+          iconWidget: Image.asset('assets/confirmation.png')),
     ];
+
     return Scaffold(
       appBar: const GenericAppBar(
         title: 'Order Details',
@@ -56,148 +70,27 @@ class OrdersDetailsScreen extends StatelessWidget {
             padding: const EdgeInsets.symmetric(horizontal: xxxTinierSpacing),
             child: Column(
               children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Container(
-                            height: 40,
-                            width: 40,
-                            decoration: const BoxDecoration(
-                                color: AppColor.primaryLighter,
-                                shape: BoxShape.circle),
-                            child: const Icon(
-                              Icons.shopping_bag_outlined,
-                              color: AppColor.primary,
-                            )),
-                        const SizedBox(
-                          width: xxxTinySpacing,
-                        ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(data.orderId.toString(),
-                                style: Theme.of(context).textTheme.headingTiny),
-                            const SizedBox(height: xxxTiniestSpacing),
-                            Row(
-                              children: [
-                                Text(
-                                  'Items: ',
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .subHeadingLarge,
-                                ),
-                                Text(
-                                  data.itemCount.toString(),
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .subHeadingLarge
-                                      .copyWith(color: AppColor.primary),
-                                ),
-                                const SizedBox(width: xxxTinierSpacing),
-                                Text(
-                                  'Total Price: ',
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .subHeadingLarge,
-                                ),
-                                Text(
-                                  ' ₹${data.totalPrice.toString()}',
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .subHeadingLarge
-                                      .copyWith(color: AppColor.primary),
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-                const SizedBox(height: tinierSpacing),
-                ListView.separated(
-                    physics: const BouncingScrollPhysics(),
-                    shrinkWrap: true,
-                    itemCount: 5,
-                    separatorBuilder: (context, index) => const Divider(
-                          height: smallestSpacing,
-                        ),
-                    itemBuilder: (context, index) {
-                      return Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Row(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: [
-                              Image.asset(
-                                orderData[index]['image'],
-                                width: kIconWidth,
-                                height: kIconHeight,
-                              ),
-                              const SizedBox(
-                                width: xxxTinySpacing,
-                              ),
-                              Column(
-                                mainAxisSize: MainAxisSize.max,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Row(
-                                    children: [
-                                      Text(orderData[index]['title'],
-                                          style: (orderData[index]['status'] !=
-                                                  'pending')
-                                              ? Theme.of(context)
-                                                  .textTheme
-                                                  .textMedium
-                                              : Theme.of(context)
-                                                  .textTheme
-                                                  .textMedium
-                                                  .copyWith(
-                                                      color: AppColor.grey)),
-                                      const SizedBox(width: xxxTinierSpacing),
-                                      (orderData[index]['status'] != 'pending')
-                                          ? const Icon(
-                                              Icons.check,
-                                              color: AppColor.primary,
-                                              size: 15,
-                                            )
-                                          : const SizedBox()
-                                    ],
-                                  ),
-                                  const SizedBox(height: xxxTiniestSpacing),
-                                  Text(
-                                    (orderData[index]['status'] != 'pending')
-                                        ? DateFormat('MMM dd y hh:mm a')
-                                            .format(data.orderPlaced!)
-                                            .toString()
-                                        : 'Pending',
-                                    style: Theme.of(context)
-                                        .textTheme
-                                        .subHeadingMedium,
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                          (orderData[index]['status'] == 'current')
-                              ? Text(
-                                  'Check My Package',
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .xxSmall
-                                      .copyWith(color: AppColor.primary),
-                                )
-                              : const SizedBox()
-                        ],
-                      );
-                    })
+                Theme(
+                  data: Theme.of(context)
+                      .copyWith(dividerColor: AppColor.transparent),
+                  child: ExpansionTile(
+                    title: const Text('Order Details'),
+                    trailing: const Icon(Icons.keyboard_arrow_down),
+                    children: [
+                      AnotherStepper(
+                        stepperList: stepperData,
+                        stepperDirection: Axis.vertical,
+                        iconWidth: kCacheImageWidth,
+                        iconHeight: kCacheImageHeight,
+                        activeBarColor: AppColor.primary,
+                        inActiveBarColor: Colors.grey,
+                        verticalGap: xxxSmallestSpacing,
+                        activeIndex: 1,
+                        barThickness: kStepper,
+                      ),
+                    ],
+                  ),
+                )
               ],
             ),
           )),
