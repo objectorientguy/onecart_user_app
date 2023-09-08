4eda541eedecd5ae7a3075941a565777f59ad15d
Ketan <kambleketan601@gmail.com>
2023-09-08 18:19:49 +0530

    OC-130: Implemented order details screen.

--- lib/Screens/orders/orders_screen.dart
+++ lib/Screens/orders/orders_screen.dart
@@ -1,191 +1,155 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:intl/intl.dart';
+import 'package:onecart_user_app/common_widgets/generic_app_bar.dart';
 import 'package:onecart_user_app/configs/app_spacing.dart';
 import 'package:onecart_user_app/configs/app_theme.dart';
-
 import '../../blocs/orders/orders_bloc.dart';
 import '../../blocs/orders/orders_events.dart';
 import '../../blocs/orders/orders_states.dart';
 import '../../configs/app_color.dart';
-import 'order_details_screen.dart';
+import '../../configs/app_dimensions.dart';
 
 class OrdersScreen extends StatelessWidget {
   const OrdersScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    List orderData = [
+      {
+        'title': 'Order Placed',
+        'deliveryDate': 'Delivered,Aug 1',
+        'image': 'assets/imgC5.png',
+        'category': 'Healthcare',
+        'quantity': 43
+      },
+      {
+        'title': 'Delivered',
+        'deliveryDate': 'Delivered,Jun 12',
+        'image': 'assets/imgC4.png',
+        'category': 'Groceries',
+        'quantity': 12
+      },
+      {
+        'title': 'Cancelled by Operations',
+        'deliveryDate': 'Delivered,Dec 25',
+        'image': 'assets/imgC3.png',
+        'category': 'Staples',
+        'quantity': 14
+      },
+      {
+        'title': 'Returned',
+        'deliveryDate': 'Delivered,Feb 14',
+        'image': 'assets/imgC2.png',
+        'category': 'Dairy Product',
+        'quantity': 20
+      },
+    ];
     context.read<GetAllOrdersBloc>().add(GetAllOrders());
     return WillPopScope(
       onWillPop: () async => false,
       child: Scaffold(
-        appBar: AppBar(
-          title:
-              Text("Orders", style: Theme.of(context).textTheme.headingMedium),
+        appBar: const GenericAppBar(
+          title: "My Orders",
+          actions: [
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: smallestSpacing),
+              child: Row(
+                children: [
+                  Icon(Icons.tune_outlined, color: AppColor.black),
+                  SizedBox(width: smallestSpacing),
+                  Icon(
+                    Icons.shopping_cart_rounded,
+                    color: AppColor.black,
+                  )
+                ],
+              ),
+            )
+          ],
         ),
-        body: Padding(
-          padding: const EdgeInsets.symmetric(
-              horizontal: leftRightMargin, vertical: topBottomPadding),
-          child: BlocBuilder<GetAllOrdersBloc, OrdersStates>(
-            builder: (context, state) {
-              if (state is GetAllOrdersLoading) {
-                return const Column(
-                  children: [
-                    SizedBox(
-                      height: 200,
-                    ),
-                    Center(child: CircularProgressIndicator()),
-                  ],
-                );
-              } else if (state is GetAllOrdersLoaded) {
-                return ListView.separated(
+        body: BlocBuilder<GetAllOrdersBloc, OrdersStates>(
+          builder: (context, state) {
+            if (state is GetAllOrdersLoading) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (state is GetAllOrdersLoaded) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: leftRightMargin, vertical: topBottomPadding),
+                child: ListView.builder(
                     physics: const BouncingScrollPhysics(),
                     shrinkWrap: true,
-                    itemCount: state.getAllOrdersListModel.data!.length,
-                    separatorBuilder: (context, index) => const Divider(
-                          height: smallestSpacing,
-                          thickness: 2,
-                        ),
+                    itemCount: orderData.length,
                     itemBuilder: (context, index) {
                       return Padding(
                         padding: const EdgeInsets.symmetric(
-                            horizontal: xxxTinierSpacing),
-                        child: InkWell(
-                          onTap: () {
-                            Navigator.pushNamed(
-                                context, OrdersDetailsScreen.routeName,
-                                arguments:
-                                    state.getAllOrdersListModel.data![index]);
-                          },
+                            horizontal: xxxTinierSpacing,
+                            vertical: xxxTinierSpacing),
+                        child: Container(
+                          width: kSizedBoxInfinite,
+                          height: kSizedBoxHeightLarge,
+                          decoration: BoxDecoration(
+                            border: Border.all(color: AppColor.lightGrey),
+                            borderRadius: BorderRadius.circular(kGeneralBorderRadius),
+                          ),
                           child: Row(
                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: [
                               Row(
-                                crossAxisAlignment: CrossAxisAlignment.start,
                                 children: [
-                                  Container(
-                                      height: 40,
-                                      width: 40,
-                                      decoration: const BoxDecoration(
-                                          color: AppColor.primaryLighter,
-                                          shape: BoxShape.circle),
-                                      child: const Icon(
-                                        Icons.shopping_bag_outlined,
-                                        color: AppColor.primary,
-                                      )),
-                                  const SizedBox(
-                                    width: xxxTinySpacing,
+                                  Padding(
+                                    padding:
+                                        const EdgeInsets.symmetric(horizontal: xxxTinierSpacing),
+                                    child: SizedBox(
+                                        height: kTextboxHeightSmallest,
+                                        width: kTextboxHeightSmallest,
+                                        child:
+                                            Image.asset(orderData[index]['image'])),
                                   ),
                                   Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    mainAxisAlignment: MainAxisAlignment.center,
                                     children: [
+                                      (orderData[index]['title'] == "Returned" ||
+                                          orderData[index]['title'] ==
+                                              "Cancelled by Operations")
+                                          ? Text(
+                                        orderData[index]['title'],
+                                        style: Theme.of(context).textTheme.orderDetailsHeadingRed,
+                                      )
+                                          : Text(orderData[index]['title'],
+                                          style: Theme.of(context).textTheme.orderDetailsHeadingRed.copyWith(color: AppColor.darkGrey,)),
+                                      (orderData[index]['title'] == "Delivered")
+                                          ? Text(orderData[index]['deliveryDate'],
+                                          style: Theme.of(context).textTheme.orderDetailsDeliveryStatus)
+                                          : const SizedBox(),
                                       Text(
-                                          state.getAllOrdersListModel
-                                              .data![index].orderId
-                                              .toString(),
-                                          style: Theme.of(context)
-                                              .textTheme
-                                              .headingTiny),
-                                      const SizedBox(height: xxxTiniestSpacing),
-                                      Row(
-                                        children: [
-                                          Text(
-                                            'Ordered On: ',
-                                            style: Theme.of(context)
-                                                .textTheme
-                                                .subHeadingLarge,
-                                          ),
-                                          SizedBox(
-                                            child: Text(
-                                              DateFormat('MMM dd y')
-                                                  .format(state
-                                                      .getAllOrdersListModel
-                                                      .data![index]
-                                                      .orderPlaced!)
-                                                  .toString(),
-                                              style: Theme.of(context)
-                                                  .textTheme
-                                                  .subHeadingLarge
-                                                  .copyWith(
-                                                      color: AppColor.primary),
-                                            ),
-                                          ),
-                                        ],
-                                      ),
-                                      Row(
-                                        children: [
-                                          Text(
-                                            'Items: ',
-                                            style: Theme.of(context)
-                                                .textTheme
-                                                .subHeadingLarge,
-                                          ),
-                                          Text(
-                                            state.getAllOrdersListModel
-                                                .data![index].itemCount
-                                                .toString(),
-                                            style: Theme.of(context)
-                                                .textTheme
-                                                .subHeadingLarge
-                                                .copyWith(
-                                                    color: AppColor.primary),
-                                          ),
-                                          const SizedBox(
-                                              width: xxxTinierSpacing),
-                                          Text(
-                                            'Total Price: ',
-                                            style: Theme.of(context)
-                                                .textTheme
-                                                .subHeadingLarge,
-                                          ),
-                                          Text(
-                                            '₹${state.getAllOrdersListModel.data![index].totalPrice.toString()}',
-                                            style: Theme.of(context)
-                                                .textTheme
-                                                .subHeadingLarge
-                                                .copyWith(
-                                                    color: AppColor.primary),
-                                          ),
-                                        ],
-                                      ),
+                                          "${orderData[index]['category']} (${orderData[index]['quantity']} items)",
+                                          style: Theme.of(context).textTheme.orderDetailsDeliveryStatus.copyWith(color: AppColor.darkGrey)),
                                     ],
                                   ),
                                 ],
                               ),
-                              const Center(
-                                child: Icon(
-                                  Icons.keyboard_arrow_right,
-                                  color: AppColor.primary,
+                              const Padding(
+                                padding: EdgeInsets.symmetric(horizontal: tinierSpacing),
+                                child: Row(
+                                  children: [
+                                    Icon(Icons.arrow_forward_ios_rounded,color: AppColor.grey,size: kCounterIcon),
+                                  ],
                                 ),
-                              ),
-                              // Container(
-                              //     padding: const EdgeInsets.symmetric(
-                              //         horizontal: 2, vertical: 2),
-                              //     decoration: BoxDecoration(
-                              //       border: Border.all(
-                              //           color: Colors.green, width: 1.0),
-                              //       borderRadius: BorderRadius.circular(20),
-                              //     ),
-                              //     child: const Center(
-                              //       child: Icon(
-                              //         Icons.keyboard_arrow_right,
-                              //         color: AppColor.primary,
-                              //       ),
-                              //     )),
+                              )
+
                             ],
                           ),
                         ),
                       );
-                    });
-              }
-              if (state is GetAllOrdersError) {
-                return Container();
-              } else {
-                return const SizedBox();
-              }
-            },
-          ),
+                    }),
+              );
+            }
+            if (state is GetAllOrdersError) {
+              return Container();
+            } else {
+              return const SizedBox();
+            }
+          },
         ),
       ),
     );
