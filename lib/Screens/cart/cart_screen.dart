1d19ef285a1270cbd85e257bba782e2d8d130f1b
Sameer <gptsameer7@gmail.com>
2023-09-12 10:24:46 +0530

    OC:132-Implement fixes

--- lib/Screens/cart/cart_screen.dart
+++ lib/Screens/cart/cart_screen.dart
@@ -2,21 +2,30 @@ import 'package:flutter/material.dart';
 import 'package:onecart_user_app/Screens/cart/widgets/cart_bottom_bar.dart';
 import 'package:onecart_user_app/Screens/cart/widgets/cart_item_list.dart';
 import 'package:onecart_user_app/configs/app_spacing.dart';
-
-import '../../common_widgets/generic_app_bar.dart';
+import 'package:onecart_user_app/configs/app_theme.dart';
 
 class CartScreen extends StatelessWidget {
   static const routeName = 'CartScreen';
+
   const CartScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: GenericAppBar(
-        title: 'Your Cart',
+    return Scaffold(
+      appBar: AppBar(
+        title: Padding(
+          padding: const EdgeInsets.only(left: 4),
+          child: Text(
+            "Your Cart",
+            style: Theme.of(context)
+                .textTheme
+                .tiny
+                .copyWith(fontWeight: FontWeight.w500),
+          ),
+        ),
       ),
-      bottomNavigationBar: CartBottomBar(),
-      body: Column(
+      bottomNavigationBar: const CartBottomBar(),
+      body: const Column(
         children: [
           SizedBox(
             height: tinySpacing,
