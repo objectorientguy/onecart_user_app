8d699ee001d4f696f8dc07aab022f9ade073f940
Sameer <gptsameer7@gmail.com>
2023-09-13 18:45:01 +0530

    OC:138-Implementing cart Screen

--- lib/Screens/cart/widgets/cart_header.dart
+++ lib/Screens/cart/widgets/cart_header.dart
@@ -12,100 +12,56 @@ class CartHeader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(
-          leftRightMargin, tinySpacing, leftRightMargin, topBottomPadding),
-      child: Column(
-        children: [
-          Row(
-            children: [
-              const CircleAvatar(
-                radius: kCircleAvatarRadius,
-                backgroundImage: AssetImage('assets/img.png'),
-              ),
-              const SizedBox(
-                width: xxTinierSpacing,
-              ),
-              Text('BRP Foods Agro Store',
+    return Container(
+      decoration: BoxDecoration(
+          color: AppColor.skyBlue,
+          borderRadius: BorderRadius.circular(kBigRadius)),
+      padding: const EdgeInsets.symmetric(
+          vertical: xxTinierSpacing, horizontal: xxxTinySpacing),
+      child: Column(children: [
+        Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+          Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            Text('Subtotal', style: Theme.of(context).textTheme.tinier),
+            const SizedBox(height: xxxTinierSpacing),
+            Row(crossAxisAlignment: CrossAxisAlignment.end, children: [
+              Text('₹',
                   style: Theme.of(context)
                       .textTheme
-                      .tiny
+                      .xxMedium
+                      .copyWith(fontWeight: FontWeight.w100)),
+              Text('2,199',
+                  style: Theme.of(context)
+                      .textTheme
+                      .xxMedium
                       .copyWith(fontWeight: FontWeight.w700)),
+              Text('.43',
+                  style: Theme.of(context)
+                      .textTheme
+                      .xxTiny
+                      .copyWith(color: AppColor.mediumBlack)),
               const SizedBox(
-                width: xxxTinySpacing,
+                width: xxxTinierSpacing,
               ),
-              const Icon(
-                Icons.arrow_forward_ios_outlined,
-                color: AppColor.primary,
+              Container(
+                decoration: BoxDecoration(
+                    border: Border.all(
+                      color: AppColor.grey,
+                    ),
+                    color: AppColor.white,
+                    borderRadius: BorderRadius.circular(kBigRadius)),
+                padding: const EdgeInsets.symmetric(
+                    vertical: xxTiniestSpacing, horizontal: xxTinierSpacing),
+                child: Center(
+                  child: Text('3 ITEMS',
+                      style: Theme.of(context).textTheme.xxTinier.copyWith(
+                          color: AppColor.primary,
+                          fontWeight: FontWeight.w500)),
+                ),
               )
-            ],
-          ),
-          const SizedBox(
-            height: xxxTinySpacing,
-          ),
-          Container(
-            padding: const EdgeInsets.all(leftRightMargin),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(12),
-              color: AppColor.skyBlue,
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text('Subtotal',
-                    style: Theme.of(context)
-                        .textTheme
-                        .tinier
-                        .copyWith(color: AppColor.grey)),
-                const SizedBox(height: xxxTinierSpacing),
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    Text('₹',
-                        style: Theme.of(context)
-                            .textTheme
-                            .xxMedium
-                            .copyWith(fontWeight: FontWeight.w100)),
-                    Text('2,199',
-                        style: Theme.of(context)
-                            .textTheme
-                            .xxMedium
-                            .copyWith(fontWeight: FontWeight.w700)),
-                    Text('.43',
-                        style: Theme.of(context)
-                            .textTheme
-                            .xxTiny
-                            .copyWith(color: AppColor.mediumBlack)),
-                    const SizedBox(
-                      width: xxxTinierSpacing,
-                    ),
-                    Container(
-                      decoration: BoxDecoration(
-                          border: Border.all(
-                            color: AppColor.grey,
-                          ),
-                          color: AppColor.primaryLight,
-                          borderRadius: BorderRadius.circular(15)),
-                      padding: const EdgeInsets.symmetric(
-                          vertical: xxTiniestSpacing,
-                          horizontal: xxTinierSpacing),
-                      child: Center(
-                        child: Text('3 ITEMS',
-                            style: Theme.of(context)
-                                .textTheme
-                                .textSmall
-                                .copyWith(
-                                    color: AppColor.primary,
-                                    fontWeight: FontWeight.w700)),
-                      ),
-                    ),
-                  ],
-                )
-              ],
-            ),
-          )
-        ],
-      ),
+            ])
+          ])
+        ])
+      ]),
     );
   }
 }
