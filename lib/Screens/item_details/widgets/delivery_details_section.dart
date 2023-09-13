fbf6535f3813b79b9df509c05e213fa693f7ebd4
Sameer <gptsameer7@gmail.com>
2023-09-13 13:37:54 +0530

    OC:135-Implement feature list in product details screen

--- lib/Screens/item_details/widgets/delivery_details_section.dart
+++ lib/Screens/item_details/widgets/delivery_details_section.dart
@@ -9,35 +9,22 @@ class DeliverDetailsSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    List deliveryData = [
+      {'title': '7 day returns', 'image': 'assets/cod.png'},
+      {'title': 'Same day shipping', 'image': 'assets/delivery_truck.png'},
+      {'title': 'Cancellable', 'image': 'assets/fast_delivery.png'},
+      {'title': 'Cancellable', 'image': 'assets/return_order.png'},
+      {'title': 'Cancellable', 'image': 'assets/same_day.png'},
+    ];
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        SizedBox(
+      children: List.generate(deliveryData.length, (index) {
+        return SizedBox(
             width: kImage,
             child: Image.asset(
-              'assets/cod.png',
-            )),
-        SizedBox(
-            width: kImage,
-            child: Image.asset(
-              'assets/delivery_truck.png',
-            )),
-        SizedBox(
-            width: kImage,
-            child: Image.asset(
-              'assets/fast_delivery.png',
-            )),
-        SizedBox(
-            width: kImage,
-            child: Image.asset(
-              'assets/return_order.png',
-            )),
-        SizedBox(
-            width: kImage,
-            child: Image.asset(
-              'assets/same_day.png',
-            )),
-      ],
+              deliveryData[index]['image'],
+            ));
+      }),
     );
   }
 }
