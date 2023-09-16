b219745fc462e19629dbbe3581040388ec71fedf
Sameer <gptsameer7@gmail.com>
2023-09-16 11:26:49 +0530

    OC:141-Implementing select variant flow

--- lib/blocs/view_cart_bloc/view_cart_events.dart
+++ lib/blocs/view_cart_bloc/view_cart_events.dart
@@ -1,3 +1,7 @@
 abstract class ViewCartEvent {}
 
-class GetAllCartItems extends ViewCartEvent {}
+class GetAllCartItems extends ViewCartEvent {
+  final int cartItemId;
+
+  GetAllCartItems(this.cartItemId);
+}
