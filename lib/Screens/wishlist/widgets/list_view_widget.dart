bf89e01046bc2ac3703056acb7d1f7fbe039d0a9
Sameer <gptsameer7@gmail.com>
2023-09-23 15:32:22 +0530

    OC:151-Add changes in wishlist screen

--- lib/Screens/wishlist/widgets/list_view_widget.dart
+++ lib/Screens/wishlist/widgets/list_view_widget.dart
@@ -22,83 +22,93 @@ class ListViewScreen extends StatelessWidget {
                 borderRadius: BorderRadius.circular(smallCardCurve)),
             padding: const EdgeInsets.symmetric(
                 vertical: xxxTinierSpacing, horizontal: xxxSmallestSpacing),
-            child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Center(
-                      child: Padding(
-                          padding: const EdgeInsets.all(tiniestSpacing),
-                          child: Container(
-                              height: kCardHeightItem,
-                              width: kWidth,
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(
-                                      kBorderRadiusSmall)),
-                              child: Image.network(
-                                  wishlistData[index].image[0].toString(),
-                                  fit: BoxFit.fill)))),
-                  const SizedBox(width: xxxTinySpacing),
-                  Expanded(
-                    child: SizedBox(
-                      height: kHeight,
-                      child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Row(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Expanded(
-                                      child: Text(
-                                          wishlistData[index].productName,
+            child: Stack(children: [
+              Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Center(
+                        child: Padding(
+                            padding: const EdgeInsets.all(tiniestSpacing),
+                            child: Container(
+                                height: kCardHeightItem,
+                                width: kWidth,
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(
+                                        kBorderRadiusSmall)),
+                                child: Image.network(
+                                    wishlistData[index].image[0].toString(),
+                                    fit: BoxFit.fill)))),
+                    const SizedBox(width: xxxTinySpacing),
+                    Expanded(
+                      child: SizedBox(
+                        height: kHeight,
+                        child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Row(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Expanded(
+                                        child: Text(
+                                            wishlistData[index].productName,
+                                            style: Theme.of(context)
+                                                .textTheme
+                                                .xxTinier
+                                                .copyWith(
+                                                    fontWeight: FontWeight.w500,
+                                                    color: AppColor.black),
+                                            maxLines: 2,
+                                            overflow: TextOverflow.ellipsis,
+                                            softWrap: false)),
+                                  ]),
+                              Row(children: [
+                                Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      SizedBox(
+                                        width: kTextboxHeightSmall,
+                                        child: Text(
+                                          wishlistData[index].quantity,
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .tiniest
+                                              .copyWith(
+                                                  color: AppColor.primary),
+                                          maxLines: 1,
+                                          overflow: TextOverflow.ellipsis,
+                                        ),
+                                      ),
+                                      const SizedBox(height: xxxTiniestSpacing),
+                                      Text(
+                                          '₹${wishlistData[index].discountedCost}',
                                           style: Theme.of(context)
                                               .textTheme
                                               .xxTinier
                                               .copyWith(
                                                   fontWeight: FontWeight.w500,
-                                                  color: AppColor.black),
-                                          maxLines: 2,
-                                          overflow: TextOverflow.ellipsis,
-                                          softWrap: false)),
-                                ]),
-                            Row(children: [
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  SizedBox(
-                                    width: kTextboxHeightSmall,
-                                    child: Text(
-                                      wishlistData[index].quantity,
-                                      style: Theme.of(context)
-                                          .textTheme
-                                          .tiniest
-                                          .copyWith(color: AppColor.primary),
-                                      maxLines: 1,
-                                      overflow: TextOverflow.ellipsis,
-                                    ),
-                                  ),
-                                  const SizedBox(height: xxxTiniestSpacing),
-                                  Text('₹${wishlistData[index].discountedCost}',
-                                      style: Theme.of(context)
-                                          .textTheme
-                                          .xxTinier
-                                          .copyWith(
-                                              fontWeight: FontWeight.w500,
-                                              color: AppColor.lightestGrey)),
-                                ],
-                              ),
-                              const ButtonWidget(
-                                title: 'Add to Cart',
-                                showRatingScreen: true,
-                              ),
-                            ])
-                          ]),
-                    ),
-                  )
-                ]),
+                                                  color:
+                                                      AppColor.lightestGrey)),
+                                    ]),
+                                const ButtonWidget(
+                                  title: 'Add to Cart',
+                                  showRatingScreen: true,
+                                ),
+                              ])
+                            ]),
+                      ),
+                    )
+                  ]),
+              const Positioned(
+                right: kZero,
+                child: Icon(Icons.close,
+                    size: kIconSizeSmall, color: AppColor.grey),
+              )
+            ]),
           );
         },
         separatorBuilder: (BuildContext context, int index) => const SizedBox(
