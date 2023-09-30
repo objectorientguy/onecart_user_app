97fbb3f329f6c6cd2aa0878fd0fe1e0c9bd2d52d
Sameer <gptsameer7@gmail.com>
2023-09-30 10:59:38 +0530

    OC:160-Implement internet handling and add to cart flow

--- lib/data/models/home/home_model.dart
+++ lib/data/models/home/home_model.dart
@@ -68,6 +68,7 @@ class Category {
   final String categoryName;
 
   Category({
+    required this.categoryName,
     required this.categoryId,
     required this.categoryImage,
   });
@@ -108,87 +109,3 @@ class PopularShop {
         "shop_image": shopImage,
       };
 }
-
-// class TodaySDeal {
-//   final int productId;
-//   final String productName;
-//   final String details;
-//   final List<Variant> variants;
-//
-//   TodaySDeal({
-//     required this.productId,
-//     required this.productName,
-//     required this.details,
-//     required this.variants,
-//   });
-//
-//   factory TodaySDeal.fromJson(Map<String, dynamic> json) => TodaySDeal(
-//     productId: json["product_id"],
-//     productName: json["product_name"],
-//     details: json["details"],
-//     variants: List<Variant>.from(json["variants"].map((x) => Variant.fromJson(x))),
-//   );
-//
-//   Map<String, dynamic> toJson() => {
-//     "product_id": productId,
-//     "product_name": productName,
-//     "details": details,
-//     "variants": List<dynamic>.from(variants.map((x) => x.toJson())),
-//   };
-// }
-//
-// class Variant {
-//   final int variantId;
-//   final int variantCost;
-//   final int count;
-//   final String brandName;
-//   final int cartItemQuantityCount;
-//   final int discountedCost;
-//   final int discount;
-//   final String quantity;
-//   final String description;
-//   final List<String> image;
-//   final int ratings;
-//
-//   Variant({
-//     required this.variantId,
-//     required this.variantCost,
-//     required this.count,
-//     required this.brandName,
-//     required this.cartItemQuantityCount,
-//     required this.discountedCost,
-//     required this.discount,
-//     required this.quantity,
-//     required this.description,
-//     required this.image,
-//     required this.ratings,
-//   });
-//
-//   factory Variant.fromJson(Map<String, dynamic> json) => Variant(
-//     variantId: json["variant_id"],
-//     variantCost: json["variant_cost"],
-//     count: json["count"],
-//     brandName: json["brand_name"],
-//     cartItemQuantityCount: json["cart_item_quantity_count"],
-//     discountedCost: json["discounted_cost"],
-//     discount: json["discount"],
-//     quantity: json["quantity"],
-//     description: json["description"],
-//     image: List<String>.from(json["image"].map((x) => x)),
-//     ratings: json["ratings"],
-//   );
-//
-//   Map<String, dynamic> toJson() => {
-//     "variant_id": variantId,
-//     "variant_cost": variantCost,
-//     "count": count,
-//     "brand_name": brandName,
-//     "cart_item_quantity_count": cartItemQuantityCount,
-//     "discounted_cost": discountedCost,
-//     "discount": discount,
-//     "quantity": quantity,
-//     "description": description,
-//     "image": List<dynamic>.from(image.map((x) => x)),
-//     "ratings": ratings,
-//   };
-// }
